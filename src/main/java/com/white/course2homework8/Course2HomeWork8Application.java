0e1875774cbf2a4662f464639468cc77519ce632
Sergey White <sergey_white86@mail.ru>
2024-03-11 22:57:52 +0300

    init commit

---
+++ src/main/java/com/white/course2homework8/Course2HomeWork8Application.java
@@ -0,0 +1,14 @@
+package com.white.course2homework8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2HomeWork8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course2HomeWork8Application.class, args);
+
+	}
+
+}
